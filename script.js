17f85cfc7b89b57091e4ad20a8b166e9398521ee
Jessie Kong <jessielkong@gmail.com>
2021-10-18 20:31:35 -0400

    createDots function

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ var config = {
 };
 
 var player;
-var stars;
+var dots;
 var bombs;
 var platforms;
 var cursors;
-var score = 20;
+var score = 0;
 var gameOver = false;
 var scoreText;
 
@@ -33,7 +33,9 @@ function preload ()
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
+    this.load.image('dot', 'assets/dot.png');
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+
 }
 
 function create ()
@@ -41,6 +43,7 @@ function create ()
     //  A simple background for our game
     this.add.image(400, 300, 'sky');
 
+    
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = this.physics.add.staticGroup();
 
@@ -55,6 +58,7 @@ function create ()
 
     // The player and its settings
     player = this.physics.add.sprite(100, 450, 'dude');
+
 
     //  Player physics properties. Give the little guy a slight bounce.
     player.setCollideWorldBounds(true);
@@ -82,35 +86,24 @@ function create ()
 
     //  Input Events
     cursors = this.input.keyboard.createCursorKeys();
-
-    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-    stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
-    });
-
-    stars.children.iterate(function (child) {
-
-        //  Give each star a slightly different bounce
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-    });
-
     bombs = this.physics.add.group();
+
+    //  DOTS
+    //  The dots are 4 by 4, evenly spaced 20 pixels apart in the x or y direction
+    dots = createDots(this);
+
 
     //  The score
     scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-    //  Collide the player and the stars with the platforms
+    //  Collide the player and the bombs with the platforms
     this.physics.add.collider(player, platforms);
-    this.physics.add.collider(stars, platforms);
     this.physics.add.collider(bombs, platforms);
 
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    this.physics.add.overlap(player, stars, collectStar, null, this);
-
     this.physics.add.collider(player, bombs, hitBomb, null, this);
+
+    //  Checks to see if the player overlaps with any of the dots, if he does call the eatDot function
+    this.physics.add.overlap(player, dots, eatDot, null, this);
 }
 
 function update ()
@@ -147,23 +140,22 @@ function update ()
     }
     else
     {
-
         player.anims.play('turn');
     }
 }
 
-function collectStar (player, star)
+function eatDot (player, dot)
 {
-    star.disableBody(true, true);
+    dot.disableBody(true, true);
 
     //  Add and update the score
     score += 10;
     scoreText.setText('Score: ' + score);
 
-    if (stars.countActive(true) === 0)
+    if (dots.countActive(true) === 0)
     {
-        //  A new batch of stars to collect
-        stars.children.iterate(function (child) {
+        //  A new batch of dots to collect
+        dots.children.iterate(function (child) {
 
             child.enableBody(true, child.x, 0, true, true);
 
@@ -189,4 +181,13 @@ function hitBomb (player, bomb)
     player.anims.play('turn');
 
     gameOver = true;
+}
+
+function createDots (realThis) {
+    dots = realThis.physics.add.group({
+        key: 'dot',
+        repeat: 10,
+        setXY: { x: 2, y: 100, stepX: 20, stepY: 0 },
+    });
+    return dots;
 }
