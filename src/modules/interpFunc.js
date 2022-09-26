dfc377e58a3b062a71994e5f76984d5af6b05234
Yoxmo <mamo.17@buckeyemail.osu.edu>
2022-09-25 21:32:54 -0400

    [*] minor fixes

--- src/modules/interpFunc.js
+++ src/modules/interpFunc.js
@@ -3,6 +3,16 @@ import moveObj from './moveFunc.js'
 
 const codeEditor = document.getElementById('code-editor');
 
+/*
+
+0 = pink 
+1 = red
+2 = blue
+3 = yellow
+
+*/
+
+
 const theme = {
     "&": {
         color: "black",
@@ -15,6 +25,27 @@ const theme = {
 
 C4C.Editor.create(codeEditor, theme);
 
+
+
+// Get text from editor
+
+/* 
+
+var newText == text from editor;
+
+function on codeEdit(){
+
+    if(newText.startsWith('example') || newText.includes('key Phrases')){
+        C4C function to move ghost...
+    } else {
+        return 'Sorry that code is... try looking at the help section on the right!'; 
+    }
+}
+
+
+*/
+
+//======
 
 C4C.Interpreter.define("moveUp", () => {
     ghosts.children.iterate((child) => {
@@ -52,3 +83,24 @@ C4C.Interpreter.define("rotate", () => {
     })
 });
 
+
+// =====
+
+
+
+C4C.Interpreter.define("moveRedLeft", (item) => {
+    var redChild = ghosts.children.entries[1]
+    moveObj.setNextMove(redChild, 'x' , -item);
+});
+C4C.Interpreter.define("moveRedRight", (item) => {
+    var redChild = ghosts.children.entries[1]
+    moveObj.setNextMove(redChild, 'x' , item);
+});
+C4C.Interpreter.define("moveRedUp", (item) => {
+    var redChild = ghosts.children.entries[1]
+    moveObj.setNextMove(redChild, 'y' , -item);
+});
+C4C.Interpreter.define("moveRedDown", (item) => {
+    var redChild = ghosts.children.entries[1]
+    moveObj.setNextMove(redChild, 'y' , item);
+});
