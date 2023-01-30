597b488aad85ab07d5aec4ac8d964dd31cddb22a
YM <94254616+Yoxmo@users.noreply.github.com>
2023-01-30 17:27:06 -0500

    update

--- temp.js
+++
@@ -1,7 +0,0 @@
-const { execFile } = require('node:child_process');
-const child = execFile('sh', ['build.sh'], (error, stdout, stderr) => {
-  if (error) {
-    throw error;
-  }
-  console.log(stdout);
-});
